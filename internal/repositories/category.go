a0117920fc27d7f3165421a4d8eafc401f96bb77
artyom <kandlerr3@gmail.com>
2024-07-03 14:55:16 +0300

    get categories for CategoryRepository

--- internal/repositories/category.go
+++ internal/repositories/category.go
@@ -29,3 +29,20 @@ func (r *CategoryRepository) CreateCategory(ctx context.Context, category *domai
 	_, err := r.db.Exec(ctx, "INSERT INTO category (title) VALUES ($1)", category.Title)
 	return err
 }
+
+func (r *CategoryRepository) GetCategories(ctx context.Context) (*[]domain.Category, error) {
+	var categories []domain.Category
+	sqlString := "SELECT id, title FROM categories"
+	rows, err := r.db.Query(ctx, sqlString)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var category domain.Category
+		err := rows.Scan(&category.ID, &category.Title)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &categories, nil
+}
