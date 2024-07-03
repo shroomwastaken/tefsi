cfa1b578e54580aa591127a63ee3cee9408f0cd0
ImNetheN <sky08ninja@gmail.com>
2024-07-03 12:35:20 +0300

    implemented category service

--- internal/services/category.go
+++ internal/services/category.go
@@ -7,7 +7,27 @@ import (
 )
 
 type CategoryRepository interface {
-	CreateGategory(ctx context.Context, category *domain.Category) error
+	CreateCategory(ctx context.Context, category *domain.Category) error
 	GetCategoryByID(ctx context.Context, id int) (*domain.Category, error)
-	GetCategories(ctx context.Context) ([]domain.Category, error)
+	GetCategories(ctx context.Context) (*[]domain.Category, error)
+}
+
+type CategoryService struct {
+	repo CategoryRepository
+}
+
+func NewDefaultCategoryService(repo CategoryRepository) *CategoryService {
+	return &CategoryService{repo: repo}
+}
+
+func (s *CategoryService) CreateCategory(ctx context.Context, category *domain.Category) error {
+	return s.repo.CreateCategory(ctx, category)
+}
+
+func (s *CategoryService) GetCategoryByID(ctx context.Context, id int) (*domain.Category, error) {
+	return s.repo.GetCategoryByID(ctx, id)
+}
+
+func (s *CategoryService) GetCategories(ctx context.Context) (*[]domain.Category, error) {
+	return s.repo.GetCategories(ctx)
 }
