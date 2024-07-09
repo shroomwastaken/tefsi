e7159fe822906ee198c122126ed2b6204e163b4b
ImNetheN <sky08ninja@gmail.com>
2024-07-09 13:07:55 +0300

    renamed files and added CreateRepos in tests

---
+++ tests/init_test.go
@@ -0,0 +1,27 @@
+package tests
+
+import (
+	"context"
+	"testing"
+)
+
+func TestCreateContainer(t *testing.T) {
+	container, db, err := CreateContainer("test-db")
+	if err != nil {
+		t.Fatal("couldnt create container:\n", err)
+	}
+	defer db.Close()
+	defer container.Terminate(context.Background())
+
+	_, err = db.Exec(context.Background(), "CREATE TABLE testtable ()")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCreateRepos(t *testing.T) {
+	_, err := CreateRepos()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
